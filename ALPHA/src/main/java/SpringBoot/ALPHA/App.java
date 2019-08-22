fb9fab0ec9e140f459b8d96e30405baa3ee1f351
olNYXlo <48833626+olNYXlo@users.noreply.github.com>
2019-08-22 10:36:03 +0800

    10.35am 22/08/19   first commit

---
+++ ALPHA/src/main/java/SpringBoot/ALPHA/App.java
@@ -0,0 +1,13 @@
+package SpringBoot.ALPHA;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
